14a851d21b102147f058d8f5094e9b8838347851
Krzysztof Wolarz <krzysztofwolarz@gmail.com>
2021-02-09 23:19:59 +0100

    configure set enviroment

---
+++ src/utils/scripts/set-enviroment.js
@@ -0,0 +1,6 @@
+#!/bin/node
+const fs = require("fs");
+
+const environment = process.argv[2]
+const envFileContent = require(`../envs/${environment}.json`);
+fs.writeFileSync("env.json", JSON.stringify(envFileContent, undefined, 2));
